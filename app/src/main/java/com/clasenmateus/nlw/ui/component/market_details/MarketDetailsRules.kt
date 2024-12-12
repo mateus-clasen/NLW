41106ac4dc05181a28685419f2e2195109f8775b
mateus-clasen <mateushc2@hotmail.com>
2024-12-12 11:38:57 -0300

    fifth commit

---
+++ app/src/main/java/com/clasenmateus/nlw/ui/component/market_details/MarketDetailsRules.kt
@@ -0,0 +1,84 @@
+package com.clasenmateus.nlw.ui.component.market_details
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.clasenmateus.nlw.R
+import com.clasenmateus.nlw.data.model.Market
+import com.clasenmateus.nlw.ui.theme.Gray400
+import com.clasenmateus.nlw.ui.theme.Gray500
+import com.clasenmateus.nlw.ui.theme.Typography
+
+/**
+ * Created by Mateus H. Clasen on 12/12/2024.
+ */
+
+@Composable
+fun MarketDetailsRules(modifier: Modifier = Modifier, market: Market) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(text = "Regulamento", style = Typography.headlineSmall, color = Gray400)
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = R.drawable.ic_ticket),
+                    tint = Gray500,
+                    contentDescription = "Ícone Cupons"
+                )
+                Text(
+                    text = "${market.coupons} cupons disponíveis",
+                    style = Typography.labelMedium,
+                    color = Gray500
+                )
+
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = R.drawable.ic_map_pin),
+                    tint = Gray500,
+                    contentDescription = "Ícone Endereço"
+                )
+                Text(
+                    text = market.address,
+                    style = Typography.labelMedium,
+                    color = Gray500
+                )
+
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = R.drawable.ic_phone),
+                    tint = Gray500,
+                    contentDescription = "Ícone Telefone"
+                )
+                Text(
+                    text = market.phone,
+                    style = Typography.labelMedium,
+                    color = Gray500
+                )
+
+            }
+
+        }
+    }
+}
